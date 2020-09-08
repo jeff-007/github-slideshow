a65fb6f1474801e1551bfe43acf608b41a306444
shanmao <wjs6298582@163.com>
2020-09-08 22:18:53 +0800

    vue.config配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,37 @@
+'use strict'
+const path = require('path')
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
 module.exports = {
-  lintOnSave: false
+  /**
+   * You will need to set publicPath if you plan to deploy your site under a sub path,
+   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
+   * then publicPath should be set to "/bar/".
+   * In most cases please use '/' !!!
+   * Detail: https://cli.vuejs.org/config/#publicpath
+   */
+  publicPath: process.env.NODE_ENV === 'development' ? './' : './',
+  lintOnSave: false,
+  outputDir: '../axios_demo/pack_demo',
+  assetsDir: 'static',
+  indexPath: 'index.html',
+  productionSourceMap: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
+  css: {
+    extract: true,
+    sourceMap: false,
+    requireModuleExtension: false,
+    loaderOptions: {
+      scss: {
+        prependData: `@import "~@/styles/variables.scss";`
+      }
+    }
+  },
 }
