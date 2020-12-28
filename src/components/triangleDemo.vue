9ca4b075d8265173068f10b78643723d02a8dda0
shanmao <wjs6298582@163.com>
2020-12-28 23:20:35 +0800

    鼠标点击选择场景中的物体demo

--- src/components/triangleDemo.vue
+++ src/components/triangleDemo.vue
@@ -26,6 +26,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import * as Stats from 'three/examples/js/libs/stats.min.js'
 import * as dat from 'three/examples/jsm/libs/dat.gui.module.js'
 import { Lensflare } from 'three/examples/jsm/objects/Lensflare.js'
+import { Projector } from 'three/examples/jsm/renderers/Projector.js'
 
 export default {
   name: 'TriangleDemo',
@@ -40,9 +41,206 @@ export default {
   computed: {},
   created() {},
   mounted() {
-    this.initPoints()
+    this.getSelectObject()
   },
   methods: {
+    //  使用Projector和Three.Raycaster来检测是否使用鼠标点击了某个对象
+    getSelectObject() {
+      const stats = initStats();
+
+      // create a scene, that will hold all our elements such as objects, cameras and lights.
+      const scene = new Three.Scene();
+
+      // create a camera, which defines where we're looking at.
+      const camera = new Three.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+      // create a render and set the size
+      const renderer = new Three.WebGLRenderer();
+
+      renderer.setClearColor(new Three.Color(0xEEEEEE, 1.0));
+      renderer.setSize(window.innerWidth, window.innerHeight);
+
+      document.addEventListener('mousedown', onDocumentMouseDown, false);
+      document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+      // create the ground plane
+      const planeGeometry = new Three.PlaneGeometry(60, 20, 1, 1);
+      const planeMaterial = new Three.MeshLambertMaterial({ color: 0xffffff });
+      const plane = new Three.Mesh(planeGeometry, planeMaterial);
+      plane.receiveShadow = true
+
+      plane.rotation.x = -0.5 * Math.PI;
+      plane.position.x = 15;
+      plane.position.y = 0;
+      plane.position.z = 0;
+
+      // add the plane to the scene
+      scene.add(plane);
+
+      // create a cube
+      const cubeGeometry = new Three.BoxGeometry(4, 4, 4);
+      const cubeMaterial = new Three.MeshLambertMaterial({ color: 0xff0000 });
+      const cube = new Three.Mesh(cubeGeometry, cubeMaterial);
+      cube.castShadow = true
+
+      // position the cube
+      cube.position.x = -9;
+      cube.position.y = 3;
+      cube.position.z = 0;
+
+      // add the cube to the scene
+      scene.add(cube);
+
+      const sphereGeometry = new Three.SphereGeometry(4, 20, 20);
+      const sphereMaterial = new Three.MeshLambertMaterial({ color: 0x7777ff });
+      const sphere = new Three.Mesh(sphereGeometry, sphereMaterial);
+
+      // position the sphere
+      sphere.position.x = 20;
+      sphere.position.y = 0;
+      sphere.position.z = 2;
+      // add the sphere to the scene
+      scene.add(sphere);
+
+      const cylinderGeometry = new Three.CylinderGeometry(2, 2, 20);
+      const cylinderMaterial = new Three.MeshLambertMaterial({ color: 0x77ff77 });
+      const cylinder = new Three.Mesh(cylinderGeometry, cylinderMaterial);
+
+      cylinder.position.set(0, 0, 1);
+
+      scene.add(cylinder);
+
+
+      // position and point the camera to the center of the scene
+      camera.position.x = -30;
+      camera.position.y = 40;
+      camera.position.z = 30;
+      camera.lookAt(scene.position);
+
+      // add subtle ambient lighting
+      const ambientLight = new Three.AmbientLight(0x0c0c0c);
+      scene.add(ambientLight);
+
+      // add spotlight for the shadows
+      const spotLight = new Three.SpotLight(0xffffff);
+      spotLight.position.set(-40, 60, -10);
+
+      scene.add(spotLight);
+
+      // add the output of the renderer to the html element
+      document.getElementById('container').appendChild(renderer.domElement);
+
+      // call the render function
+      let step = 0;
+      let scalingStep = 0;
+
+      const controls = new function() {
+        this.rotationSpeed = 0.02;
+        this.bouncingSpeed = 0.03;
+        this.scalingSpeed = 0.03;
+        this.showRay = false;
+      }();
+
+      let gui = new dat.GUI();
+      gui.add(controls, 'rotationSpeed', 0, 0.5);
+      gui.add(controls, 'bouncingSpeed', 0, 0.5);
+      gui.add(controls, 'scalingSpeed', 0, 0.5);
+      gui.add(controls, 'showRay').onChange(function(e) {
+        if (tube) scene.remove(tube)
+      });
+
+      render();
+
+      function render() {
+        stats.update();
+        // rotate the cube around its axes
+        cube.rotation.x += controls.rotationSpeed;
+        cube.rotation.y += controls.rotationSpeed;
+        cube.rotation.z += controls.rotationSpeed;
+
+        // bounce the sphere up and down
+        step += controls.bouncingSpeed;
+        sphere.position.x = 20 + (10 * (Math.cos(step)));
+        sphere.position.y = 2 + (10 * Math.abs(Math.sin(step)));
+
+        // scale the cylinder
+
+        scalingStep += controls.scalingSpeed;
+        let scaleX = Math.abs(Math.sin(scalingStep / 4));
+        let scaleY = Math.abs(Math.cos(scalingStep / 5));
+        let scaleZ = Math.abs(Math.sin(scalingStep / 7));
+        cylinder.scale.set(scaleX, scaleY, scaleZ);
+
+        // render using requestAnimationFrame
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+
+      }
+
+      let projector = new Projector();
+      let tube;
+
+      function onDocumentMouseDown(event) {
+
+        let vector = new Three.Vector3((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1, 0.5);
+        vector = vector.unproject(camera);
+
+        let raycaster = new Three.Raycaster(camera.position, vector.sub(camera.position).normalize());
+
+        let intersects = raycaster.intersectObjects([sphere, cylinder, cube]);
+
+        if (intersects.length > 0) {
+
+          console.log(intersects[0]);
+
+          intersects[0].object.material.transparent = true;
+          intersects[0].object.material.opacity = 0.1;
+        }
+      }
+
+      function onDocumentMouseMove(event) {
+        if (controls.showRay) {
+          let vector = new Three.Vector3((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1, 0.5);
+          vector = vector.unproject(camera);
+
+          let raycaster = new Three.Raycaster(camera.position, vector.sub(camera.position).normalize());
+          let intersects = raycaster.intersectObjects([sphere, cylinder, cube]);
+
+          if (intersects.length > 0) {
+
+            let points = [];
+            points.push(new Three.Vector3(-30, 39.8, 30));
+            points.push(intersects[0].point);
+
+            let mat = new Three.MeshBasicMaterial({ color: 0xff0000, transparent: true, opacity: 0.6 });
+            let tubeGeometry = new Three.TubeGeometry(new Three.SplineCurve3(points), 60, 0.001);
+
+            if (tube) scene.remove(tube);
+
+            if (controls.showRay) {
+              tube = new Three.Mesh(tubeGeometry, mat);
+              scene.add(tube);
+            }
+          }
+        }
+      }
+
+      function initStats() {
+
+        let stats = new Stats();
+
+        stats.setMode(0); // 0: fps, 1: ms
+
+        // Align top-left
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.left = '0px';
+        stats.domElement.style.top = '0px';
+
+        document.getElementById('container').appendChild(stats.domElement);
+
+        return stats;
+      }
+    },
     //  pointCloud
     initPoints() {
       const stats = initStats();
@@ -65,7 +263,6 @@ export default {
 
       // add the output of the renderer to the html element
       document.getElementById('container').appendChild(webGLRenderer.domElement);
-
 
       createParticles();
       render();
